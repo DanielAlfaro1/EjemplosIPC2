e7ec7dbfbfb35640baf9953b61dd6bef0cad5fef
Daniel Arturo Alfaro Gaitan <36682590+DanielAlfaro1@users.noreply.github.com>
2023-01-21 10:22:26 -0600

    Ejemplo básico de Python

---
+++ Clase 1/main.py
@@ -0,0 +1,17 @@
+print("hola mundo")
+
+print("Que tal compañeros, esta es la clase de IPC2")
+
+if True:
+    print("Entramos al IF")
+    print("Me gustan los IF")
+print("Estamos fuera del IF")
+
+variable1 = 0
+variable2 = 12
+print(variable1)
+print(variable1 + 1)
+variable1 = "10"
+print(variable1)
+print(variable1 + str(variable2))
+print(int(variable1) + variable2)
